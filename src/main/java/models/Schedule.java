58d148179b2eaee0085ff4523ab9cac45687aba0
Ashkan Forghani <ashkanf@hotmail.com>
2024-11-07 16:31:27 -0500

    Updated Classes
    -Schedule class
    -TimeSlot class

--- src/main/java/models/Schedule.java
+++ src/main/java/models/Schedule.java
@@ -1,5 +1,52 @@
 package models;
 
+import java.util.List;
+
 public class Schedule {
 
+    //Instance variables
+    private int scheduleID;  
+    private List<TimeSlot> timeSlots;
+
+    //Constructor
+    public Schedule(int scheduleID, List<TimeSlot> timeSlots) {
+        this.scheduleID = scheduleID;
+        this.timeSlots = timeSlots;
+    }
+
+    //Getters and setters
+    public int getScheduleID() {
+        return scheduleID;
+    }
+
+    public void setScheduleId(int scheduleID) {
+        this.scheduleID = scheduleID;
+    }
+
+    public List<TimeSlot> getTimeSlots() {
+        return timeSlots;
+    }
+
+    public void setTimeSlots(List<TimeSlot> timeSlots) {
+        this.timeSlots = timeSlots;
+    }
+
+    // Method to add a TimeSlot to the Schedule
+    public void addTimeSlot(TimeSlot timeSlot) {
+        this.timeSlots.add(timeSlot);
+    }
+
+    // Method to remove a TimeSlot from the Schedule
+    public void removeTimeSlot(TimeSlot timeSlot) {
+        this.timeSlots.remove(timeSlot);
+    }
+
+    @Override
+    public String toString() {
+        return "Schedule{" +
+                "scheduleId=" + scheduleID +
+                ", timeSlots=" + timeSlots +
+                '}';
+    }
 }
+
--- src/main/java/models/TimeSlot.java
+++ src/main/java/models/TimeSlot.java
@@ -1,5 +1,63 @@
 package models;
 
+import java.time.LocalDateTime;
+
 public class TimeSlot {
 
+    //Instance variables
+    private int timeSlotID;  
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+
+    //Constructor
+    public TimeSlot(int timeSlotID, LocalDateTime startTime, LocalDateTime endTime, LocalDateTime startDate, LocalDateTime endDate) {
+        this.timeSlotID = timeSlotID;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    //Getters and setters
+    public int getTimeSlotID() {
+        return timeSlotID;
+    }
+
+    public void setTimeSlotId(int timeSlotID) {
+        this.timeSlotID = timeSlotID;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDateTime getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDateTime endDate) {
+        this.endDate = endDate;
+    }
 }
