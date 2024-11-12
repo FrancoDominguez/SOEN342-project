b470879424640ffd708af7b59b9a71f04c26ced2
FrancoDominguez <franco@wearecws.com>
2024-11-12 14:55:28 -0500

    merging

--- src/main/java/lessonbooking/models/Account.java
+++ src/main/java/lessonbooking/models/Account.java
@@ -1,4 +1,5 @@
-packagelessonbooking.models; 
+package lessonbooking.models;
+
 import java.util.ArrayList;
 import java.util.UUID;
 import lessonbooking.services.Mysqlcon;
@@ -24,19 +25,20 @@ public abstract class Account {
     // this will add the new account object to the list of accounts
     // whether that list is in the db or elsewhere
 
-        //Possible implementation where registeredAccounts would be in the DB
-        // Check if the account is already registered
-        
-        /*for (Account account : registeredAccounts) {
-          if (account.phoneNumber.equals(this.phoneNumber)) {
-            System.out.println("Account with this phone number already exists.");
-            return;
-          }
-        }
-        
-        // Add the new account to the list
-        registeredAccounts.add(this);
-       */
+    // Possible implementation where registeredAccounts would be in the DB
+    // Check if the account is already registered
+
+    /*
+     * for (Account account : registeredAccounts) {
+     * if (account.phoneNumber.equals(this.phoneNumber)) {
+     * System.out.println("Account with this phone number already exists.");
+     * return;
+     * }
+     * }
+     * 
+     * // Add the new account to the list
+     * registeredAccounts.add(this);
+     */
   };
 
   public void login() {
